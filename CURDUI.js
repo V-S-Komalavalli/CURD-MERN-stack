df33ddbd471a915917fe6d4685acdc106d28d118
V-S-Komalavalli <111239654+V-S-Komalavalli@users.noreply.github.com>
2023-08-05 21:14:25 +0530

    Add files via upload

--- CURDUI.js
+++ CURDUI.js
@@ -1,42 +1,59 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-export default class CURDUI extends Component
-{
-constructor(props)
-{
- super(props)
- this.state = {
-    empid:''
- }
-this.handleSubmit=this.handleSubmit.bind(this)
-this.handleChange=this.handleChange.bind(this)
-}
- handleChange(event)
-{
- this.setState({[event.target.name]:event.target.value})
-}
- handleSubmit(event)
-{
-event.preventDefault();
-console.log(this.state)
-axios.post('http://localhost:5000/selectemployee',this.state)
-.then(response=>
-{
-console.log(response.data);
-document.getElementById("op").innerHTML=response.data;
-this.setState({res:response.data})
-})
-.catch(error=>console.log(error)) }
- render()
-{ return (
-<div>
- <form onSubmit={this.handleSubmit}>
- <h1> Enter EMPID</h1> <input type="text" name="empid" onChange={this.handleChange}/> <br/>
- 
-<br/> <input type="submit"/>
-</form>
-<div id="op"></div>
-</div>
-)
-}
+import React, { Component } from 'react';
+import axios from 'axios';
+
+export default class CURDUI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      empid: '',
+      res: '',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    axios
+      .post('http://localhost:5000/selectemployee', this.state)
+      .then((response) => {
+        console.log(response.data);
+        this.setState({ res: response.data });
+      })
+      .catch((error) => console.log(error));
+  }
+
+  render() {
+    return (
+      <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <div className="w-full max-w-md p-6 bg-white rounded-lg shadow-md">
+          <h1 className="text-2xl font-semibold mb-4 text-center">Enter EMPID</h1>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              name="empid"
+              className="w-full border border-gray-300 p-2 rounded-md mb-4 focus:outline-none focus:border-blue-500"
+              onChange={this.handleChange}
+            />
+            <button
+              type="submit"
+              className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 transition"
+            >
+              Submit
+            </button>
+          </form>
+          <div
+            id="op"
+            className="mt-4 p-2 border border-gray-300 rounded-md bg-gray-50"
+          >
+            {this.state.res}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
